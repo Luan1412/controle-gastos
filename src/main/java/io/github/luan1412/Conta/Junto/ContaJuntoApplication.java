14d5ef32757a4842fa5bb725faa3434e5c5cd235
Luan1412 <luanzimhp@gmail.com>
2025-10-07 14:14:15 -0300

    init: Configuração inicial do projeto com Spring Boot

---
+++ src/main/java/io/github/luan1412/Conta/Junto/ContaJuntoApplication.java
@@ -0,0 +1,13 @@
+package io.github.luan1412.Conta.Junto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaJuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaJuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/luan1412/Conta/Junto/ContaJuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luan1412.Conta.Junto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaJuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
